f56982a858f89a1e32639d79859159f514815d20
Prahalad Singh Gaur <singhgaurprahalad@gmail.com>
2025-02-18 11:36:15 +0530

    milestone 17

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const product = require("./product");
 
 const userSchema = new mongoose.Schema({
   name:{
@@ -56,6 +57,21 @@ const userSchema = new mongoose.Schema({
       required: true,
     },
  },
+ cart: [
+  {
+    productId : {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true
+    },
+    quantity:{
+      type: Number,
+      required: true,
+      min: [1,"Quantity cannot be less than 1"],
+      default: 1
+    }
+  }
+ ],
  createdAt:{
   type: Date,
   default: Date.now(),
